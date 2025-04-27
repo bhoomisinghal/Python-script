9ff415f174478d6170b3587fde1e6b45aa4e2265
bhoomisinghal <bhoomisinghal123@gmail.com>
2025-04-27 11:41:53 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,12 @@
+a=input("Enter the first value:")
+b=input("Enter the secound value:")
+a=int(a)
+b=int(b)
+ad=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("addition:",ad)
+print("subtraction:",sub)
+print("multiplication:",mul)
+print("division:",div)
