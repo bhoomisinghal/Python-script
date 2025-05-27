9b08645939d07f755bb516743482e5abfbe87197
bhoomisinghal <bhoomisinghal123@gmail.com>
2025-05-27 16:23:18 +0530

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,5 @@
+a=int(input("enter the integer number:"))
+if a%2==0:
+    print("its a even number:",a)
+else:
+    print("it os a odd number:",a)
---
+++ task2.py
@@ -0,0 +1,5 @@
+l = list(range(1, 51))
+total = 0
+for i in l:
+    total += i
+print("Total sum of (1 to 50):", total)
